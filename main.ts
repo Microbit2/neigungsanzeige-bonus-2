c260f57148f93907425e1f6b2bd972fa953c2e1f
Microbit2 <95872969+Microbit2@users.noreply.github.com>
2021-12-09 18:30:37 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,38 @@
+input.onButtonPressed(Button.A, function () {
+    if (input.acceleration(Dimension.X) < 0) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (input.acceleration(Dimension.X) > 0) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (input.acceleration(Dimension.Y) < 0) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (input.acceleration(Dimension.Y) > 0) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+})
